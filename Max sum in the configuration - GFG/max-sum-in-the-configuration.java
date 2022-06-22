7585cd67c685ba32952f417b603a1d897b7af59d
Deep Patel <52374016+deeppatel99@users.noreply.github.com>
2022-06-22 15:24:57 +0530

    Added solution - LeetHub

--- Max sum in the configuration - GFG/max-sum-in-the-configuration.java
+++ Max sum in the configuration - GFG/max-sum-in-the-configuration.java
@@ -29,17 +29,18 @@ class GfG
     int max_sum(int A[], int n)
     {
          int preWsum = 0 , Totsum = 0;
-     for(int i=0;i<n;i++){
-         preWsum+=A[i]*i;
-         Totsum+=A[i];
-     }
-     int ans = preWsum;
-     for(int i=1;i<n;i++){
-         int temp = preWsum-(Totsum-A[i-1])+A[i-1]*(n-1);
-         ans = Math.max(ans,temp);
-         preWsum = temp;
-     }
-     return ans;
+         for(int i=0;i<n;i++){
+             preWsum+=A[i]*i;
+             Totsum+=A[i];
+         }
+         int ans = preWsum;
+         for(int i=0;i<n-1;i++){
+            //  int temp = preWsum-(Totsum-A[i-1])+A[i-1]*(n-1);
+             int temp = preWsum+Totsum - n * A[n- i - 1];
+             ans = Math.max(ans,temp);
+             preWsum = temp;
+         }
+         return ans;
     }	
     
    
