bbc932d91eb146c8c1cc6363eac5520bc8d02a6f
Deep Patel <52374016+deeppatel99@users.noreply.github.com>
2022-06-23 23:22:33 +0530

    Added solution - LeetHub

--- Reorganize The Array - GFG/reorganize-the-array.java
+++ Reorganize The Array - GFG/reorganize-the-array.java
@@ -51,41 +51,17 @@ class Complete{
     // Function for finding maximum and value pair
     public static int[] Rearrange (int A[], int n) {
         // Complete the function
+        Set<Integer> s = new HashSet<>();
         for (int i = 0; i < A.length; i++)
         {
-            if (A[i] != -1 && A[i] != i)
-            {
-                int x = A[i];
- 
-                // check if desired place
-                // is not vacate
-                while (A[x] != -1 && A[x] != x)
-                {
-                    // store the value from
-                    // desired place
-                    int y = A[x];
- 
-                    // place the x to its correct
-                    // position
-                    A[x] = x;
- 
-                    // now y will become x, now
-                    // search the place for x
-                    x = y;
-                }
- 
-                // place the x to its correct
-                // position
-                A[x] = x;
- 
-                // check if while loop hasn't
-                // set the correct value at A[i]
-                if (A[i] != i)
-                {
-                    // if not then put -1 at
-                    // the vacated place
-                    A[i] = -1;
-                }
+            s.add(A[i]);
+        }
+        
+        for(int i = 0; i < A.length; i++){
+            if(s.contains(i)){
+                A[i] = i;
+            }else{
+                A[i] = -1;
             }
         }
         return A;
